f6e590d7abd2d79e97f376757cb1b08409139798
Yasin BUYUKGUZEL <116117219+YasinBG@users.noreply.github.com>
2023-11-08 13:47:05 +0300

    tailwind import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    
+    <p class="text-3xl text-blue-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus fugiat modi tempore tempora praesentium pariatur, error sunt aut accusantium harum voluptatem minima quasi, quisquam iste aliquam commodi assumenda fuga. Fugiat!</p>
   </div>
 </template>
 
