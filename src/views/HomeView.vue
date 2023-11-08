21e7ffd073178888276b0f48d52cd6e68c822527
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-11-08 14:48:47 +0300

    html olusturuluyor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="home">
-    <p class="text-3xl text-blue-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus fugiat modi tempore tempora praesentium pariatur, error sunt aut accusantium harum voluptatem minima quasi, quisquam iste aliquam commodi assumenda fuga. Fugiat!</p>
-  </div>
+  <div>
+
+<input class="w-60 h-12 border-2 border-green-500 rounded mx-auto font-semibold"/>
+
+<ul class="flex flex-col my-6">
+    <li class="flex text-start my-2 font-semibold">
+        <input type="text">
+        <span></span>
+        <div>
+            <button class="border-red-800 border-2 text-center uppercase rounded bg-red-400 text-white w-12 h-6 font-semibold">sil</button>
+        </div>
+    </li>
+</ul>
+
+<button class=" font-semibold border-2 border-green-600 text-center uppercase rounded bg-green-400 text-white hover:bg-green-600">geri al</button>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 
 
 export default {
