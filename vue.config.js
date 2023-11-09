5651bf3d0f7eb8c747fa830e1de293a27e3cf354
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-11-09 20:11:29 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, './docs'),
-  publicPath: process.env.NODE_ENV !== 'development' ? '/todo' : '/',
+  publicPath: process.env.NODE_ENV !== 'development' ? '/todo-list' : '/',
 
 })
